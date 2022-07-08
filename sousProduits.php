8222dd57c2d4865504ec1c88494eaaf9576f1c43
Valentin Mignon <valentin54112@gmail.com>
2022-07-08 12:22:05 +0200

    v2.7

---
+++ sousProduits.php
@@ -0,0 +1,149 @@
+<?php require('./assets/components/header.php'); ?>
+
+<h2>NOS T-SHIRTS</h2>
+
+
+<section class="troisParties">
+
+    <div class="titrePartie">
+        SELECTION "1ER PRIX"
+    </div>
+    <div class="cinqModule">
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+    </div>
+    <div class="titrePartie">
+        MEILLEUR RAPPORT QUALITÉ/PRIX
+    </div>
+    <div class="cinqModule">
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+    </div>
+    <div class="titrePartie">
+        SELECTION "PREMIUM"
+    </div>
+    <div class="cinqModule">
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+        <div class="carte" style="width: 15rem;">
+            <img src="./assets/images/tshirtTest.jpg" class="card-img-top" alt="..." class="cardImg">
+            <div class="card-body">
+                <div class="titreCard">REFERENCE</div>
+                <div class="descCard">DESCRIPTION</div>
+                <div class="prixCard">à partir de x€ (hors marquage)</div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+
+<?php require('./assets/components/footer.php'); ?>
+<?php require('./assets/components/footer.php'); ?>
