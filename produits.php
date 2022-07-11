6f46a451bf81b2448e3e0a209af43f429557150e
Valentin Mignon <valentin54112@gmail.com>
2022-07-11 13:21:18 +0200

    v2.9

---
+++ produits.php
@@ -0,0 +1,172 @@
+<?php
+require('./assets/components/header.php'); ?>
+
+
+<div class="produitstexte">
+    <div class="produittexte">
+        <h2><strong>TITRE</strong></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignis-
+            sim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+            eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae,
+            consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras</p>
+    </div>
+</div>
+<div class="titreCorporate">
+    <h1><strong> NOS PRODUITS «CORPORATE» </strong></h1>
+</div>
+<div class="ttcartes">
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">T-SHIRTS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">POLOS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">CHEMISES</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">SWEATSHIRTS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">BODYWARMERS</h5>
+        </div>
+    </div>
+</div>
+
+<div class="ttcartes2">
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">BLOUSONS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">SOFTSHELLS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">PANTALONS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">SHORTS</h5>
+        </div>
+    </div>
+    <div class="cardproduits">
+        <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">POLAIRES</h5>
+        </div>
+    </div>
+</div>
+
+<div class="workwear">
+    <div class="titreCorporate">
+        <h1><strong>NOS PRODUITS «WORKWEAR»</strong> </h1>
+    </div>
+    <div class="ttcartes">
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">HAUTE VISIBILITÉ </h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">TECHNIQUE LINE</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">MULTIPRO</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">INDUSTRY</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">SAFETY</h5>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="accessoire">
+    <div class="titreCorporate">
+        <h1><strong> NOS PRODUITS «ACCESSOIRES»</strong> </h1>
+    </div>
+    <div class="ttcartes">
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">BONNETS & CASQUETTES</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">BAGAGERIE</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">TABLIERS</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">TOTEBAGS & POCHETTES</h5>
+            </div>
+        </div>
+        <div class="cardproduits">
+            <img src="https://fakeimg.pl/50x78/" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">ÉPONGE</h5>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="titreEurope">
+    <h1><strong>NOS PRODUITS FULL CUSTOM FABRIQUÉS EN EUROPE</strong> </h1>
+</div>
+<div class="cardproduitsultime">
+    <img src="https://fakeimg.pl/1250x350/" class="imageproduit">
+    <div class="card-body">
+        <h5 class="card-titleultime"><strong>TOUS NOS PRODUITS FABRIQUÉS À LA COMMANDE DANS NOTRE ATELIER EUROPÉEN</strong></h5>
+    </div>
+</div>
+
+
+
+
+<?php
+require('./assets/components/footer.php');
+?>
