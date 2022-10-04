da87d24a1b931195df2f3ff567468d792a246bda
Valentin Mignon <valentin54112@gmail.com>
2022-10-04 18:13:22 +0200

    v0410

--- assets/js/select.js
+++ assets/js/select.js
@@ -6,15 +6,50 @@ console.log("hello world");
 //     document.body.append
 // }
 
-let image = document.getElementsByClassName('marquage');
+// let image = document.getElementsByClassName('marquage');
 
-function modif() {
-    array.forEach(image => {
-        image.style.border='10px solid #efb74f';
-    });
+// function modif() {
+//     array.forEach(image => {
+//         image.style.border='10px solid #efb74f';
+//     });
     
+// }
+
+// function select(e) {
+// image.addEventListener('onclick' , modif(e));
+// }
+//SELECTEURS//
+const ttmarq = document.querySelector(".ttmarque")
+const marq = document.querySelectorAll(".marquage");
+
+//FONCTIONS 
+for (let i = 0; i < marq.length; i++){
+    marq[i].addEventListener("click",function(){
+        marq[i].style.border='8px solid #efb74f';
+    })
+}
+for (let i = 0; i < marq.length; i++){
+    marq[i].addEventListener("dblclick",function(e){
+        marq[i].style.border='none';
+    })
 }
 
-function select(e) {
-image.addEventListener('onclick' , modif(e));
-}
+let modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "block";
+  };
+});
+let closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
+    let modal = btn.closest(".modal");
+    modal.style.display = "none";
+  };
+});
+window.onclick = function (event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+};
